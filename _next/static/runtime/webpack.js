43417ce99479366f2b0da304b860f61a8ee7e60c
rodrigoronin <liramailbox@gmail.com>
2020-02-05 20:15:14 -0300

    Navbar com botões cinza

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ed77b3bac0eeb1ff52af";
+/******/ 	var hotCurrentHash = "3a5b3e1252d28d2665c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
