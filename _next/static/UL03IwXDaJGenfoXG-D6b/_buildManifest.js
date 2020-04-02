d3b772eb4d8ab0f3dfc6331c96767803fdc249ca
Damis Garcia <damisgarcia@gmail.com>
2020-04-02 20:46:09 -0300

    rebuild

---
+++ _next/static/UL03IwXDaJGenfoXG-D6b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/index":[a],"/projeto":[a]}}("static\u002Fchunks\u002Fe0855c41939b676345c13ded874da9eb8274efaf.60a4320c27fcbec1dc52.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
