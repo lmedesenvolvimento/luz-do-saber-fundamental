043ab6b0b67cfae7f2d8c6747ee7e31c73526ce6
Damis Garcia <damisgarcia@gmail.com>
2020-04-03 14:08:24 -0300

    remove docs folder from project

--- docs/_next/static/K_vy_IzMpoXoT2DX0avO_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/download":[a],"/index":[a],"/projeto":[a]}}("static\u002Fchunks\u002Fc2fa36ed553d5c6cac1e3d5727a1361a5b76861f.f1b11f618820a77dacc5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
