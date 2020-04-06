3e8e32ffe85aaf0ebd439df1f99f62fe1d791eb9
juniorce <fg.gomesjunior@gmail.com>
2020-04-06 12:09:23 -0300

    add route page

--- pages/download.js
+++ pages/download.js
@@ -6,35 +6,56 @@ export default function Projeto(){
     const links = [
         {
             title: 'Luz do Saber - 1º Ano - Caderno de Orientações Didáticas',
-            src: 'static/downloads/Luz do Saber - 1º Ano - Caderno de Orientações Didáticas.pdf',            
+            src: 'static/downloads/Luz do Saber - 1º Ano - Caderno de Orientações Didáticas.pdf',
         },
         {
             title: 'Luz do Saber - 1º ano',
-            src: 'static/downloads/Luz do Saber - 1º ano.pdf',            
+            src: 'static/downloads/Luz do Saber - 1º ano.pdf',
         },
         {
             title: 'Luz do Saber - 2º Ano - Caderno de Orientações Didáticas',
-            src: 'static/downloads/Luz do Saber - 2º Ano - Caderno de Orientações Didáticas.pdf',            
+            src: 'static/downloads/Luz do Saber - 2º Ano - Caderno de Orientações Didáticas.pdf',
         },
         {
             title: 'Luz do Saber - 2º ano',
-            src: 'static/downloads/Luz do Saber - 2º ano.pdf',            
+            src: 'static/downloads/Luz do Saber - 2º ano.pdf',
         },
     ]
     return (
         <div id="download">
             <BaseLayout>
                 <Header selected="download" />
-                <div className="__content">
-                    <h1 className="heading">Downloads</h1>
-                    <LinkList items={links} />
-                </div>
-                <div className="footer">
-                    <Footer />
-                </div>
+                <section className="banner full-width">
+                  <div className="pagination">
+                    <span className="pagination-item">principal</span> <span className="pagination-item">/</span> <span className="pagination-item">Downloads</span>
+                  </div>
+                  <div className="__content">
+                      <h1 className="heading">Downloads</h1>
+                      <LinkList items={links} />
+                  </div>
+                  <div className="footer">
+                      <Footer />
+                  </div>
+                </section>
             </BaseLayout>
             <style jsx>{
                 `
+                    .pagination {
+                        display: flex;
+                        align-items: center;
+                        position: absolute;
+                        background: #FFB151;
+                        border-radius: 0;
+                        width: 100%;
+                        height: 50px;
+                        padding: 0 90px;
+                        color: #fff;
+                        font-size: 0.8em;
+                        text-transform: uppercase;
+                    }
+                    .pagination-item {
+                        margin-right: 10px;
+                    }
                     #download .__content{
                         margin: 64px 48px 24px;
                     }
