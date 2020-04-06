01421694fe6490d2b862d8091a065684b72914ef
Damis Garcia <damisgarcia@gmail.com>
2020-04-06 10:05:32 -0300

    Deploy to gh-pages

---
+++ docs/_next/static/3BJ2SCTHjcRGX4t_uEe5T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/creditos":[a],"/download":[a],"/index":[a],"/projeto":[a]}}("static\u002Fchunks\u002F7c6ecbd301d264501c8b2dc4f19030e983fb6cd4.88ccf2fdf842dc201243.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
